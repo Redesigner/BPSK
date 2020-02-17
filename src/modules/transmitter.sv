25cbf1d63fe4ca6b4bb9887deb0cc8571d5eb583
Redesigner <stephenwmelnick@gmail.com>
2020-02-17 15:19:18 -0500

    Fixed transmitter, added testbench proj

--- src/modules/transmitter.sv
+++ src/modules/transmitter.sv
@@ -9,10 +9,10 @@ module transmitter
     );
     reg [7:0] uart_packet;
     uart_deserialize uart_rx(data, clk_baud, uart_packet, data_ready);
-    uart_data_buffer buffer(data_ready, data_clear, uart_packet, sys_packet, buffer_send);
+    uart_data_buffer buffer(data_ready, data_clear, clk_baud, uart_packet, sys_packet, buffer_send);
 	reg [DATA_WIDTH-1:0] signal_analog;
 	signal_modulator modulator(clk, ser_signal, buffer_send, signal_analog, ser_next);
     reg [PACKET_SIZE-1:0] sys_packet;
-    packet_serializer serialize(sys_packet, ser_next, ser_signal, data_clear);
+    packet_serializer serialize(sys_packet, clk, ser_next, ser_signal, data_clear);
 
 endmodule
